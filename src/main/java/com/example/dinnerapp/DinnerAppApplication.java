d11816accefef1205ee74b5b3e3a691c5db9fabe
wirusowy <wirusowy@users.noreply.github.com>
2022-03-23 18:12:04 +0100

    Initial commit

---
+++ src/main/java/com/example/dinnerapp/DinnerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinnerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinnerAppApplication.class, args);
+    }
+
+}
